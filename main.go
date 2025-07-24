ae266994dc58a308984697bf1343bd29609fdd81
snowsoft <abdulkadirkar@gmail.com>
2025-07-24 17:24:23 +0300

    fix: resolve final linter issues - Add error checking for remaining spinner.Start calls - Format code with gofmt - Remove deprecated github-actions output format - Add gofmt step to CI pipeline

--- main.go
+++ main.go
@@ -2,15 +2,15 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    
-    "github.com/snowsoft/codeweaver/cmd"
+	"fmt"
+	"os"
+
+	"github.com/snowsoft/codeweaver/cmd"
 )
 
 func main() {
-    if err := cmd.Execute(); err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-        os.Exit(1)
-    }
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
